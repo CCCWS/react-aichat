63af65970b5d40b64adacd1d1566ab9b14623391
CCCWS <eung0626@gmail.com>
2022-12-06 01:38:13 +0900

    shopping

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,10 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const papago_1 = __importDefault(require("./papago"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const port = 3001;
 app.use(express_1.default.json());
+app.use(express_1.default.static(path_1.default.join(__dirname, "build")));
 app.use("/api/papago", papago_1.default);
+if (process.env.NODE_ENV === "production") {
+    app.get("/*", (req, res) => {
+        res.set({
+            "Cache-Control": "no-cache, no-store, must-revalidate",
+            Pragma: "no-cache",
+            Date: Date.now(),
+        });
+        res.sendFile(path_1.default.join(__dirname, "build", "index.html"));
+    });
+}
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,25 @@
 import express, { request, response, NextFunction } from "express";
 import papago from "./papago";
+import path from "path";
+import http from "http";
 
 const app = express();
 const port = 3001;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "build")));
 app.use("/api/papago", papago);
+
+if (process.env.NODE_ENV === "production") {
+  app.get("/*", (req, res) => {
+    res.set({
+      "Cache-Control": "no-cache, no-store, must-revalidate",
+      Pragma: "no-cache",
+      Date: Date.now(),
+    });
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
