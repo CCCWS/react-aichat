717976ba0fd7f1155dac746b5a9325ce208a2e69
CCCWS <eung0626@gmail.com>
2022-12-06 03:26:55 +0900

    ai-chat

--- server/dist/index.js
+++ server/dist/index.js
@@ -13,14 +13,16 @@ app.use(express_1.default.static(path_1.default.join(__dirname, "build")));
 app.use("/api/papago", papago_1.default);
 if (process.env.NODE_ENV === "production") {
     app.get("/*", (req, res) => {
+        app.use(express_1.default.static("client/build"));
         res.set({
             "Cache-Control": "no-cache, no-store, must-revalidate",
             Pragma: "no-cache",
             Date: Date.now(),
         });
-        res.sendFile(path_1.default.join(__dirname, "build", "index.html"));
+        res.sendFile(path_1.default.join(__dirname, "../client", "build", "index.html"));
     });
 }
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
+    console.log(process.env.NODE_ENV);
 });
--- server/index.ts
+++ server/index.ts
@@ -11,15 +11,17 @@ app.use("/api/papago", papago);
 
 if (process.env.NODE_ENV === "production") {
   app.get("/*", (req, res) => {
+    app.use(express.static("client/build"));
     res.set({
       "Cache-Control": "no-cache, no-store, must-revalidate",
       Pragma: "no-cache",
       Date: Date.now(),
     });
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
   });
 }
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+  console.log(process.env.NODE_ENV);
 });
