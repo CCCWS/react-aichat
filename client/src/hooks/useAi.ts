eb0087c2471a48b49eb9c65477ee6cf117a4c9e6
CCCWS <eung0626@gmail.com>
2023-03-05 23:08:04 +0900

    ai-chat

--- client/src/hooks/useAi.ts
+++ client/src/hooks/useAi.ts
@@ -13,7 +13,7 @@ const useAi = () => {
   const onSendMessageAi = useCallback(
     (value: string, langType: "ko" | "en") => {
       const configuration = new Configuration({
-        apiKey: Ai_ApiKey,
+        apiKey: "k-XoGaXjvg2J1vY1FPVGgTT3BlbkFJFBwt0p3WuCzaBwHoc4QR",
       });
 
       const openai = new OpenAIApi(configuration);
