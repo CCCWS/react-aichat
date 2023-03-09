417ddc9b50510031b773f738e7b6f66873bc2ee9
CCCWS <eung0626@gmail.com>
2023-03-10 01:24:39 +0900

    ai-chat

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,8 +1,14 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
-import { config } from "./firebaseConfig";
 
-const firebaseConfig = config;
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "react-project-a454c.firebaseapp.com",
+  projectId: "react-project-a454c",
+  storageBucket: "react-project-a454c.appspot.com",
+  messagingSenderId: "1001484494365",
+  appId: "1:1001484494365:web:3c6443456bb6b438ddc8f4",
+};
 firebase.initializeApp(firebaseConfig);
 const firestore = firebase.firestore();
 
