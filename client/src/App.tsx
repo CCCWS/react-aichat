b7b4d804926a56937538235640135b29f4ec668b
CCCWS <eung0626@gmail.com>
2024-01-05 02:04:01 +0900

    ai-chat

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import GlobalStyle from "./Components/Style/GlobalStyle";
 import Home from "./Page/Home";
-import Test2 from "./Page/Test2";
 
 import { firestore } from "./firebase";
 
@@ -9,8 +8,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      {/* <Home /> */}
-      <Test2 />
+      <Home />
     </>
   );
 }
--- client/src/hooks/useAi.ts
+++ client/src/hooks/useAi.ts
@@ -9,7 +9,6 @@ interface Api_Keys {
   PAPAGO_CLIENT_SECRET: string;
 }
 
-
 const useAi = () => {
   const [response, setResponse] = useState<string>("");
   const { result: enToKr, papagoApi: enTranslate } = useTranslate();
